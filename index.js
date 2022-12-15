e5f03a3645f9e904b2e2be5ae8e49993cfaa0d88
Delaine Raimundo <108828746+Delaine22@users.noreply.github.com>
2022-12-15 20:27:56 +0100

    Create index.js

---
+++ index.js
@@ -0,0 +1,5 @@
+function checar(){
+    var p = window.document.getElementsByClassName('check')[0]
+    var input = window.document.getElementById('checkbox')
+    p.innerHTML='<strong> By creating an account, you agree to the <a href="#">Terms & Conditions</a> </strong>'
+}
